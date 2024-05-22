94dc39379a68a0923e72b593fcfa74cca23dcd81
Giorgi <g.mekvabishvili>
2024-05-22 14:57:25 +0400

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Giorgi Mekvabishvili on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
